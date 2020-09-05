8d62cd0542870ab5c16c56779a30b113e1b76976
Kamran Elchuzade <elchuzade@gmail.com>
2020-09-05 21:34:48 +0300

    added user routes

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const path = require('path')
+
+const users = require('./routes/api/users')
+const profiles = require('./routes/api/profiles')
+const goals = require('./routes/api/goals')
+
+const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
+// DB config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('- MongoDB Error - ', err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Use routes
+app.use('/api/users', users)
+app.use('/api/profiles', profiles)
+app.use('/api/goals', goals)
+
+// Serve static assets if in production (Heroku stuff)
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5001
+
+app.listen(port, () => console.log(`server running on port ${port}`))
---
+++ routes/api/users.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
+
+// Load Input Validation
+const validateSignupInput = require('../../validation/signup')
+const validateLoginInput = require('../../validation/login')
+const validatePasswordInput = require('../../validation/password')
+
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
+
+// @route POST api/users/signup
+// @desc Signup user
+// @access Public
+
+router.post('/signup', (req, res) => {
+  const { errors, isValid } = validateSignupInput(req.body)
+  // Check for validation errors
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
+    } else {
+      const newUser = new User({
+        email: req.body.email,
+        password: req.body.password,
+        permission: 5
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+
+          newUser.password = hash
+          new Profile({ user: newUser._id })
+            .save()
+            .then(newProfile => {
+              newUser.profile = newProfile._id
+              newUser
+                .save()
+                .then(user => 
+                  res.status(200).json({
+                    item: user,
+                    source: 'user',
+                    status: 'success',
+                    message: 'Signed up Successfully!'
+                  })
+                )
+                .catch(err => console.log(err))
+            })
+        })
+      })
+    }
+  })
+})
+
+// @route POST api/users/login
+// @desc Login user / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+  // Check for validation errors
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
+  const { email, password } = req.body
+
+  // Find user by email
+  User.findOne({ email })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        errors.email = 'User not found'
+        return res.status(404).json(errors)
+      }
+      // Check password
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            if (!user.active) {
+              errors.activation = 'Account has not been activated yet!'
+              return res.status(400).json(errors)
+            }
+            // User Matched
+            const payload = {
+              id: user.id,
+              name: user.name,
+              permission: user.permission,
+              profile: user.profile
+            } // jwt payload
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 * 24 * 365 },
+              (err, token) => {
+                if (err) {
+                  console.log(err)
+                } else {
+                  return res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                  })
+                }
+              }
+            )
+          } else {
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors)
+          }
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router
